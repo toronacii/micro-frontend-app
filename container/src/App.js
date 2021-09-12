e327b6c791b5be276c84920227c23d2f26b0b629
Jose Toro <intive.jtoro@veritran.com>
2021-09-12 00:51:22 -0300

    Add header

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,12 @@
 import { mount } from '@marketing/MarketingApp';
 import RemoteApp from './components/RemoteApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 
 export default () => {
-  return <RemoteApp mount={mount} />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <RemoteApp mount={mount} />
+    </BrowserRouter>);
 }
